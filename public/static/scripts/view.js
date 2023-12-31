e661726e09499d81976ecb31a2e14f8391dfa579
reashal <reviewcations@gmail.com>
2023-12-31 20:45:03 +0800

    fix: Click on the image to close the sidebar without opening the large image

--- public/static/scripts/view.js
+++ public/static/scripts/view.js
@@ -1,11 +1,12 @@
-let imgs = document.getElementsByTagName("article")[0].getElementsByTagName("img");
+let imgs = document.getElementsByTagName("article")[0].getElementsByTagName("img")
+let theAside = document.getElementsByTagName("main")[0]
 let img = document.getElementById('view-img')
 let box = document.getElementById('view-box')
 for (let i = 0; i < imgs.length; i++) {
     if (imgs[i].parentNode.children.length > 1)
         imgs[i].parentNode.className = "p-has-img"
     imgs[i].onclick = function () {
-        if (!imgs[i].classList.contains("view-none")) {
+        if (!imgs[i].classList.contains("view-none") && !theAside.classList.contains("aside-show")) {
             box.className = "view-box-show"
             img.src = imgs[i].src
         }
