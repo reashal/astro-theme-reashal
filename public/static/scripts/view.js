a1ebdcf9f7cf6ad92448ae35b3f7d41f9ea2ec52
reashal <ruiyuqingshan@163.com>
2026-02-25 22:37:45 +0800

    styles: moments four-grid and nine-grid imgs

--- public/static/scripts/view.js
+++ public/static/scripts/view.js
@@ -1,17 +1,15 @@
 let imgs = document.getElementsByTagName("article")[0].getElementsByTagName("img")
-let theAside = document.getElementsByTagName("main")[0]
-let img = document.getElementById('view-img')
-let box = document.getElementById('view-box')
+let mainElement = document.getElementsByTagName("main")[0]
+let viewImg = document.getElementById('view-img')
+let viewBox = document.getElementById('view-box')
 for (let i = 0; i < imgs.length; i++) {
-    if (imgs[i].parentNode.children.length > 1)
-        imgs[i].parentNode.className = "p-has-img"
     imgs[i].onclick = function () {
-        if (!imgs[i].classList.contains("view-none") && !theAside.classList.contains("aside-show")) {
-            box.className = "view-box-show"
-            img.src = imgs[i].src
+        if (!imgs[i].classList.contains("view-none") && !mainElement.classList.contains("aside-show")) {
+            viewBox.className = "view-box-show"
+            viewImg.src = imgs[i].src
         }
     }
 }
-box.onclick = function () {
-    box.className = "view-box-hide"
+viewBox.onclick = function () {
+    viewBox.className = "view-box-hide"
 }
